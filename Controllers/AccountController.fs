04b42a80e178798a95d87c44bf4da02e0aa0a64d
UglyOne <tomasz@gnatonski.com>
2020-06-24 20:26:53 +0200

    project initialized

---
+++ Controllers/AccountController.fs
@@ -0,0 +1,17 @@
+﻿namespace crowdfunding_backend.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+open Microsoft.Extensions.Logging
+
+[<ApiController>]
+[<Route("[controller]")>]
+type AccountController (logger : ILogger<AccountController>) =
+    inherit ControllerBase()
+
+    [<HttpGet>]
+    member __.Get() : string =
+        "TODO"
